4b81c10fcd229a6e2ebda9107f341cec75704f70
Erik Post <epost@dearnova.nl>
2020-04-16 14:08:56 +0200

    fix use of maven wrapper

--- quiz-service/build.sh
+++ quiz-service/build.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOCKER_IMAGE_NAME=dearnova/dn-quiz-service
 
 # Build project
-mvn -f $DIR/pom.xml clean install "$@"
+cd $DIR
+./mvnw clean install "$@"
 
 # Check if last command was successful
 if [ $? -ne 0 ]; then
@@ -12,5 +13,5 @@ if [ $? -ne 0 ]; then
 fi
 
 # Build Docker Image
-$DIR/mvnw com.google.cloud.tools:jib-maven-plugin:dockerBuild \
+./mvnw com.google.cloud.tools:jib-maven-plugin:dockerBuild \
   -Dimage=$DOCKER_IMAGE_NAME
